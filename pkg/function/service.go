85c8a1b7509c297353bbda28a478481bd77cc995
jyu6 <juanlu_yu@intuit.com>
2022-08-17 16:02:38 -0700

    add initial service
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/function/service.go
+++ pkg/function/service.go
@@ -1,1 +1,39 @@
 package function
+
+import (
+	"context"
+
+	functionpb "github.com/numaproj/numaflow-go/pkg/apis/proto/function/v1"
+)
+
+type Service struct {
+	functionpb.UnimplementedUserDefinedFunctionServer
+
+	Mapper  MapHandler
+	Reducer ReduceHandler
+}
+
+func (fs *Service) DoFn(ctx context.Context, d *functionpb.Datum) (*functionpb.DatumList, error) {
+	messages, err := fs.Mapper.HandleDo(ctx, d.GetKey(), d.GetValue())
+	if err != nil {
+		return nil, err
+	}
+	var l []*functionpb.Datum
+	for _, m := range messages.Items() {
+		l = append(l, &functionpb.Datum{
+			Key:            m.Key,
+			Value:          m.Value,
+			EventTime:      d.EventTime, // should provide a way for the user to update the event time
+			PaneInfo:       d.PaneInfo,
+			IntervalWindow: d.IntervalWindow,
+		})
+	}
+	datumList := &functionpb.DatumList{
+		Elements: l,
+	}
+	return datumList, nil
+}
+
+func (fs *Service) ReduceFn(fnServer functionpb.UserDefinedFunction_ReduceFnServer) error {
+	return nil
+}
