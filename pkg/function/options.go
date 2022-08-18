820eaae1fb008bb6ee4233ded261e08c502fd33a
jyu6 <juanlu_yu@intuit.com>
2022-08-18 08:25:28 -0700

    rename handle option
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/function/options.go
+++ pkg/function/options.go
@@ -4,20 +4,20 @@ import "time"
 
 // TODO: this options file is currently only a placeholder
 
-type handleOptions struct {
+type mapOptions struct {
 	eventTime time.Time
 }
 
-type HandleOption interface {
-	apply(*handleOptions)
+type MapOption interface {
+	apply(*mapOptions)
 }
 
 type eventTimeOption time.Time
 
-func (c eventTimeOption) apply(opts *handleOptions) {
+func (c eventTimeOption) apply(opts *mapOptions) {
 	opts.eventTime = time.Time(c)
 }
 
-func WithEventTime(c time.Time) HandleOption {
+func WithEventTime(c time.Time) MapOption {
 	return eventTimeOption(c)
 }
