3f9af8a216bdb9fe1b4eb9285fdb97dd8662beb7
jyu6 <juanlu_yu@intuit.com>
2022-08-30 15:41:57 -0700

    initial commit to refact map and reduce
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

---
+++ pkg/function/example/forward_msg/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+
+	functionsdk "github.com/numaproj/numaflow-go/pkg/function"
+	"github.com/numaproj/numaflow-go/pkg/function/server"
+)
+
+func mapHandle(_ context.Context, d functionsdk.Datum) (functionsdk.Messages, error) {
+	// directly forward the input to the output
+	key := d.Key()
+	val := d.Value()
+	eventTime := d.EventTime()
+	_ = eventTime
+	watermark := d.Watermark()
+	_ = watermark
+
+	var resultKey = key
+	var resultVal = val
+	return functionsdk.MessagesBuilder().Append(functionsdk.MessageTo(resultKey, resultVal)), nil
+}
+
+func main() {
+	server.New().RegisterMapper(functionsdk.DoFunc(mapHandle)).Start(context.Background())
+}
