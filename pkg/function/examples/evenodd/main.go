645081e3ca515456b047a29f7c565fabd1f79450
Derek Wang <whynowy@gmail.com>
2022-09-15 11:36:53 -0700

    chore: function examples (#15)
    Signed-off-by: Derek Wang <whynowy@gmail.com>

---
+++ pkg/function/examples/evenodd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"strconv"
+
+	"github.com/numaproj/numaflow-go/pkg/datum"
+	functionsdk "github.com/numaproj/numaflow-go/pkg/function"
+	"github.com/numaproj/numaflow-go/pkg/function/server"
+)
+
+func handle(_ context.Context, key string, d datum.Datum) functionsdk.Messages {
+	msg := d.Value()
+	_ = d.EventTime() // Event time is available
+	_ = d.Watermark() // Watermark is available
+	// If msg is not an integer, drop it, otherwise return it with "even" or "odd" key.
+	if num, err := strconv.Atoi(string(msg)); err != nil {
+		return functionsdk.MessagesBuilder().Append(functionsdk.MessageToDrop())
+	} else if num%2 == 0 {
+		return functionsdk.MessagesBuilder().Append(functionsdk.MessageTo("even", msg))
+	} else {
+		return functionsdk.MessagesBuilder().Append(functionsdk.MessageTo("odd", msg))
+	}
+}
+
+func main() {
+	server.New().RegisterMapper(functionsdk.MapFunc(handle)).Start(context.Background())
+}
