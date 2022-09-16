265d1467bc0c156663088d8e63f598cb479c6cee
jyu6 <juanlu_yu@intuit.com>
2022-09-16 10:28:06 -0700

    minor fix
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/sink/service.go
+++ pkg/sink/service.go
@@ -49,10 +49,8 @@ func (fs *Service) SinkFn(ctx context.Context, datumList *sinkpb.DatumList) (*si
 	var hdList []Datum
 	for _, d := range datumList.GetElements() {
 		hdList = append(hdList, &handlerDatum{
-			id:        d.GetId(),
-			value:     d.GetValue(),
-			eventTime: d.GetEventTime().EventTime.AsTime(),
-			watermark: d.GetWatermark().Watermark.AsTime(),
+			id:    d.GetId(),
+			value: d.GetValue(),
 		})
 	}
 	messages := fs.Sinker.HandleDo(ctx, hdList)
