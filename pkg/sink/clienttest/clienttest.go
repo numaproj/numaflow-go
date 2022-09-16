65d18bbed213ac38663c6d8c7dfa394a2e789df4
jyu6 <juanlu_yu@intuit.com>
2022-09-15 23:56:34 -0700

    add clienttest
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/sink/clienttest/clienttest.go
+++ pkg/sink/clienttest/clienttest.go
@@ -1,1 +1,44 @@
 package clienttest
+
+import (
+	"context"
+	"fmt"
+
+	sinkpb "github.com/numaproj/numaflow-go/pkg/apis/proto/sink/v1"
+	"github.com/numaproj/numaflow-go/pkg/apis/proto/sink/v1/sinkmock"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+// client contains the grpc client for testing.
+type client struct {
+	grpcClt sinkpb.UserDefinedSinkClient
+}
+
+// New creates a new mock client object.
+func New(c *sinkmock.MockUserDefinedSinkClient) (*client, error) {
+	return &client{c}, nil
+}
+
+// CloseConn closes the grpc client connection.
+func (c *client) CloseConn(ctx context.Context) error {
+	return nil
+}
+
+// IsReady returns true if the grpc connection is ready to use.
+func (c *client) IsReady(ctx context.Context, in *emptypb.Empty) (bool, error) {
+	resp, err := c.grpcClt.IsReady(ctx, in)
+	if err != nil {
+		return false, err
+	}
+	return resp.GetReady(), nil
+}
+
+// SinkFn applies a function to a list of datum elements.
+func (c *client) SinkFn(ctx context.Context, datumList []*sinkpb.Datum) ([]*sinkpb.Response, error) {
+	responseList, err := c.grpcClt.SinkFn(ctx, &sinkpb.DatumList{Elements: datumList})
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute c.grpcClt.MapFn(): %w", err)
+	}
+
+	return responseList.GetResponses(), nil
+}
