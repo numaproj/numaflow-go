a597f8dcf053dfd4f96bbdebcaa1319cc2fa5b53
yhl01 <Yashash_HL@intuit.com>
2022-09-20 17:08:55 +0530

    mock code for reduce client
    Signed-off-by: yhl01 <Yashash_HL@intuit.com>

--- pkg/apis/proto/function/v1/mockgen.go
+++ pkg/apis/proto/function/v1/mockgen.go
@@ -1,3 +1,4 @@
 package v1
 
 //go:generate mockgen -destination funcmock/funcmock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/function/v1 UserDefinedFunctionClient
+//go:generate mockgen -destination funcmock/reducemock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/function/v1 UserDefinedFunction_ReduceFnClient
