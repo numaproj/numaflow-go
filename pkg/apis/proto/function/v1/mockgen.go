e0d483e65322c95f5b16740922f1e2c62c24b8c1
yhl01 <Yashash_HL@intuit.com>
2022-09-21 10:13:55 +0530

    keeping mock code in one single file
    Signed-off-by: yhl01 <Yashash_HL@intuit.com>

--- pkg/apis/proto/function/v1/mockgen.go
+++ pkg/apis/proto/function/v1/mockgen.go
@@ -1,4 +1,3 @@
 package v1
 
-//go:generate mockgen -destination funcmock/funcmock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/function/v1 UserDefinedFunctionClient
-//go:generate mockgen -destination funcmock/reducemock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/function/v1 UserDefinedFunction_ReduceFnClient
+//go:generate mockgen -destination funcmock/funcmock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/function/v1 UserDefinedFunctionClient,UserDefinedFunction_ReduceFnClient
