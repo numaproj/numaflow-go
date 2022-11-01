49fecd44d82eb54a92132ced2765aee0f3e0adee
jyu6 <juanlu_yu@intuit.com>
2022-11-01 15:32:06 -0700

    fmt
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/sink/configs.go
+++ pkg/sink/configs.go
@@ -6,6 +6,6 @@ const (
 	// DefaultMaxMessageSize overrides gRPC max message size configuration
 	// https://github.com/grpc/grpc-go/blob/master/server.go#L58-L59
 	//   - defaultServerMaxReceiveMessageSize
-	//	 - defaultServerMaxSendMessageSize
+	//   - defaultServerMaxSendMessageSize
 	DefaultMaxMessageSize = 1024 * 1024 * 4
 )
