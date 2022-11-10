896a564ea7850a0b243b5a57e3b11f5cf0840986
Yashash H L <yashash_hl@intuit.com>
2022-11-10 11:52:46 +0530

    constructing and passing metadata to reduce handler
    Signed-off-by: Yashash H L <yashash_hl@intuit.com>

--- pkg/function/service_test.go
+++ pkg/function/service_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	functionpb "github.com/numaproj/numaflow-go/pkg/apis/proto/function/v1"
-	"google.golang.org/grpc/metadata"
+	grpcmd "google.golang.org/grpc/metadata"
 	"google.golang.org/protobuf/types/known/emptypb"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -66,7 +66,7 @@ func TestService_MapFn(t *testing.T) {
 			// here's a trick for testing:
 			// because we are not using gRPC, we directly set a new incoming ctx
 			// instead of the regular outgoing context in the real gRPC connection.
-			ctx := metadata.NewIncomingContext(context.Background(), metadata.New(map[string]string{DatumKey: "client"}))
+			ctx := grpcmd.NewIncomingContext(context.Background(), grpcmd.New(map[string]string{DatumKey: "client"}))
 			got, err := fs.MapFn(ctx, tt.args.d)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("MapFn() error = %v, wantErr %v", err, tt.wantErr)
