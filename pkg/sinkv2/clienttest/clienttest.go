41fd24d252161cc5967e6287276f5ed4d9b6b2b2
jyu6 <juanlu_yu@intuit.com>
2022-12-02 10:52:08 -0800

    sink v2 init
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

---
+++ pkg/sinkv2/clienttest/clienttest.go
@@ -0,0 +1,53 @@
+package clienttest
+
+import (
+	"context"
+	"fmt"
+
+	sinkpb "github.com/numaproj/numaflow-go/pkg/apis/proto/sink/v2"
+	"github.com/numaproj/numaflow-go/pkg/apis/proto/sink/v2/sinkmock"
+	"google.golang.org/protobuf/types/known/emptypb"
+)
+
+// client contains the grpc client for testing.
+type client struct {
+	grpcClt sinkpb.UserDefinedSinkClient
+}
+
+// New creates a new mock client object.
+func New(c *sinkmock.MockUserDefinedSinkClient) (*client, error) {
+	return &client{c}, nil
+}
+
+// CloseConn closes the grpc client connection.
+func (c *client) CloseConn(ctx context.Context) error {
+	return nil
+}
+
+// IsReady returns true if the grpc connection is ready to use.
+func (c *client) IsReady(ctx context.Context, in *emptypb.Empty) (bool, error) {
+	resp, err := c.grpcClt.IsReady(ctx, in)
+	if err != nil {
+		return false, err
+	}
+	return resp.GetReady(), nil
+}
+
+// SinkFn applies a function to a list of datum elements.
+func (c *client) SinkFn(ctx context.Context, datumStreamCh <-chan *sinkpb.Datum) ([]*sinkpb.Response, error) {
+	stream, err := c.grpcClt.SinkFn(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute c.grpcClt.SinkFn(): %w", err)
+	}
+	for datum := range datumStreamCh {
+		if err := stream.Send(datum); err != nil {
+			return nil, fmt.Errorf("failed to execute stream.Send(%v): %w", datum, err)
+		}
+	}
+	responseList, err := stream.CloseAndRecv()
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute stream.CloseAndRecv(): %w", err)
+	}
+
+	return responseList.GetResponses(), nil
+}
---
+++ pkg/sinkv2/configs.go
@@ -0,0 +1,11 @@
+package sink
+
+const (
+	Protocol = "unix"
+	Addr     = "/var/run/numaflow/udsink.sock"
+	// DefaultMaxMessageSize overrides gRPC max message size configuration
+	// https://github.com/grpc/grpc-go/blob/master/server.go#L58-L59
+	//   - defaultServerMaxReceiveMessageSize
+	//   - defaultServerMaxSendMessageSize
+	DefaultMaxMessageSize = 1024 * 1024 * 4
+)
