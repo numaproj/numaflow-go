b2a46ed636a95025450e2f3c51d32cc499cd1a4b
jyu6 <juanlu_yu@intuit.com>
2022-12-15 09:20:46 -0800

    add docs
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

---
+++ pkg/sink/doc.go
@@ -0,0 +1,27 @@
+// Package sink implements the server code for User Defined Sink in golang.
+// Example:
+// package main
+//
+// import (
+//
+//	"context"
+//	"fmt"
+//
+//	sinksdk "github.com/numaproj/numaflow-go/pkg/sink"
+//	"github.com/numaproj/numaflow-go/pkg/sink/server"
+//
+// )
+//
+//	func handle(ctx context.Context, datumList []sinksdk.Datum) sinksdk.Responses {
+//		result := sinksdk.ResponsesBuilder()
+//		for _, datum := range datumList {
+//			fmt.Println(string(datum.Value()))
+//			result = result.Append(sinksdk.ResponseOK(datum.ID()))
+//		}
+//		return result
+//	}
+//
+//	func main() {
+//		server.New().RegisterSinker(sinksdk.SinkFunc(handle)).Start(context.Background())
+//	}
+package sink
