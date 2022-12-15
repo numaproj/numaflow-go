9de2b5ad29bc6e2a29f9ace4073b24afc438abd0
jyu6 <juanlu_yu@intuit.com>
2022-12-15 10:21:12 -0800

    update doc
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/function/doc.go
+++ pkg/function/doc.go
@@ -1,23 +1,28 @@
 // Package function implements the server code for User Defined Function in golang.
+//
 // Example:
-// package main
-//
-// import (
-// 	"context"
-//
-// 	functionsdk "github.com/numaproj/numaflow-go/pkg/function"
-// 	"github.com/numaproj/numaflow-go/pkg/function/server"
-// )
-//
-// // Simply return the same msg
-//
-// func handle(ctx context.Context, key string, data functionsdk.Datum) functionsdk.Messages {
-// 	_ = data.EventTime() // Event time is available
-// 	_ = data.Watermark() // Watermark is available
-// 	return functionsdk.MessagesBuilder().Append(functionsdk.MessageToAll(data.Value()))
-// }
-//
-// func main() {
-// 	server.New().RegisterMapper(functionsdk.MapFunc(handle)).Start(context.Background())
-// }
+/*
+  package main
+
+  import (
+
+    "context"
+
+    functionsdk "github.com/numaproj/numaflow-go/pkg/function"
+    "github.com/numaproj/numaflow-go/pkg/function/server"
+
+  )
+
+  // Simply return the same msg
+
+  func handle(ctx context.Context, key string, data functionsdk.Datum) functionsdk.Messages {
+    _ = data.EventTime() // Event time is available
+    _ = data.Watermark() // Watermark is available
+    return functionsdk.MessagesBuilder().Append(functionsdk.MessageToAll(data.Value()))
+  }
+
+  func main() {
+    server.New().RegisterMapper(functionsdk.MapFunc(handle)).Start(context.Background())
+  }
+*/
 package function
