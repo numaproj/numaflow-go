3f61f24d4c2b63d5d0789fe28c32d96cc66d75ae
jyu6 <juanlu_yu@intuit.com>
2022-12-15 09:29:21 -0800

    refmt
    Signed-off-by: jyu6 <juanlu_yu@intuit.com>

--- pkg/function/doc.go
+++ pkg/function/doc.go
@@ -2,24 +2,22 @@
 // Example:
 // package main
 //
-//	 import (
+// import (
+// 	"context"
 //
-//		  "context"
+// 	functionsdk "github.com/numaproj/numaflow-go/pkg/function"
+// 	"github.com/numaproj/numaflow-go/pkg/function/server"
+// )
 //
-//	   functionsdk "github.com/numaproj/numaflow-go/pkg/function"
-//		  "github.com/numaproj/numaflow-go/pkg/function/server"
+// // Simply return the same msg
 //
-//	 )
+// func handle(ctx context.Context, key string, data functionsdk.Datum) functionsdk.Messages {
+// 	_ = data.EventTime() // Event time is available
+// 	_ = data.Watermark() // Watermark is available
+// 	return functionsdk.MessagesBuilder().Append(functionsdk.MessageToAll(data.Value()))
+// }
 //
-//	 // Simply return the same msg
-//
-//		func handle(ctx context.Context, key string, data functionsdk.Datum) functionsdk.Messages {
-//		  _ = data.EventTime() // Event time is available
-//		  _ = data.Watermark() // Watermark is available
-//		  return functionsdk.MessagesBuilder().Append(functionsdk.MessageToAll(data.Value()))
-//		}
-//
-//		func main() {
-//		  server.New().RegisterMapper(functionsdk.MapFunc(handle)).Start(context.Background())
-//		}
+// func main() {
+// 	server.New().RegisterMapper(functionsdk.MapFunc(handle)).Start(context.Background())
+// }
 package function
