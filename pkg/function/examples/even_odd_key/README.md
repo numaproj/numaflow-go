6aec53164ca0e883915421ea2b7b0a87b02e767e
Julie Vogelmani <julie_vogelman@intuit.com>
2023-04-03 08:20:13 -0700

    udf for creating a new key 'even' or 'odd' based on the value of the incoming key
    Signed-off-by: Julie Vogelmani <julie_vogelman@intuit.com>

---
+++ pkg/function/examples/even_odd_key/README.md
@@ -0,0 +1,4 @@
+# Even Odd Key
+
+An example User Defined Function that returns the incoming message with `even` or `odd` key depending on whether the key is even or odd.
+If the input message is not an integer, the message is dropped.
---
+++ pkg/function/examples/even_odd_key/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+
+	functionsdk "github.com/numaproj/numaflow-go/pkg/function"
+	"github.com/numaproj/numaflow-go/pkg/function/server"
+)
+
+func handle(_ context.Context, key string, d functionsdk.Datum) functionsdk.Messages {
+	msg := d.Value()
+	_ = d.EventTime() // Event time is available
+	_ = d.Watermark() // Watermark is available
+	// If msg is not an integer, drop it, otherwise return it with "even" or "odd" key.
+	if num, err := strconv.Atoi(string(key)); err != nil {
+		fmt.Println("dropping message")
+		return functionsdk.MessagesBuilder().Append(functionsdk.MessageToDrop())
+	} else if num%2 == 0 {
+		fmt.Printf("num %d is even\n", num)
+		return functionsdk.MessagesBuilder().Append(functionsdk.MessageTo("even", msg))
+	} else {
+		fmt.Printf("num %d is odd\n", num)
+		return functionsdk.MessagesBuilder().Append(functionsdk.MessageTo("odd", msg))
+	}
+}
+
+func main() {
+	server.New().RegisterMapper(functionsdk.MapFunc(handle)).Start(context.Background())
+}
