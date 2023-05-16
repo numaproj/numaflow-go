930212a52c10f1a24c199c5a05da036b2f211d9e
xdevxy <115589853+xdevxy@users.noreply.github.com>
2023-05-16 10:12:43 -0700

    chore: add map udf stream example (#59)
    Signed-off-by: Hao Hao <xdevxhao@gmail.com>

---
+++ pkg/function/examples/flatmap_stream/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"context"
+	"strings"
+
+	functionsdk "github.com/numaproj/numaflow-go/pkg/function"
+	"github.com/numaproj/numaflow-go/pkg/function/server"
+)
+
+func handle(_ context.Context, _ []string, d functionsdk.Datum, messageCh chan<- functionsdk.Message) {
+	defer close(messageCh)
+	msg := d.Value()
+	_ = d.EventTime() // Event time is available
+	_ = d.Watermark() // Watermark is available
+	// Split the msg into an array with comma.
+	strs := strings.Split(string(msg), ",")
+	for _, s := range strs {
+		messageCh <- functionsdk.NewMessage([]byte(s))
+	}
+}
+
+func main() {
+	server.New().RegisterMapperStream(functionsdk.MapStreamFunc(handle)).Start(context.Background())
+}
