b295f0131ba66baf57af776fdb42ded0aa9a1ef4
Yashash H L <yashash_hl@intuit.com>
2023-08-21 20:52:34 +0530

    fix mock package
    Signed-off-by: Yashash H L <yashash_hl@intuit.com>

--- pkg/apis/proto/reduce/v1/mockgen.go
+++ pkg/apis/proto/reduce/v1/mockgen.go
@@ -1,3 +1,3 @@
 package v1
 
-//go:generate mockgen -destination reducemock/reducemock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/reduce/v1 ReduceClient,Reduce_ReduceFnClient
+//go:generate mockgen -destination reducemock/reducemock.go -package reducemock github.com/numaproj/numaflow-go/pkg/apis/proto/reduce/v1 ReduceClient,Reduce_ReduceFnClient
