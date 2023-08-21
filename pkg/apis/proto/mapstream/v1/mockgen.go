051bacdd38ee53b0f9bcf4db323e53253e274a28
Yashash H L <yashash_hl@intuit.com>
2023-08-21 20:00:19 +0530

    mock code
    Signed-off-by: Yashash H L <yashash_hl@intuit.com>

--- pkg/apis/proto/mapstream/v1/mockgen.go
+++ pkg/apis/proto/mapstream/v1/mockgen.go
@@ -1,3 +1,3 @@
 package v1
 
-//go:generate mockgen -destination mapstreammock/mapstreammock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/mapstream/v1 MapStreamClient
+//go:generate mockgen -destination mapstreammock/mapstreammock.go -package funcmock github.com/numaproj/numaflow-go/pkg/apis/proto/mapstream/v1 MapStreamClient,MapStream_MapStreamFnClient
