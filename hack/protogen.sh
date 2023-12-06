bdc9f909b41daeb63599742acfe96d8458ecdbc3
Derek Wang <whynowy@gmail.com>
2023-12-05 23:54:28 -0800

    .
    Signed-off-by: Derek Wang <whynowy@gmail.com>

--- hack/protogen.sh
+++ hack/protogen.sh
@@ -27,15 +27,15 @@ ORG=${ORG:-numaproj}
 PROJECT=${PROJECT:-numaflow}
 BRANCH=${BRANCH:-main}
 
-REMOTE_URL="https://raw.githubusercontent.com/${ORG}/${PROJECT}/${BRANCH}/pkg/apis/proto"
-echo "REMOTE_URL: $REMOTE_URL"
+REMOTE_URL_PRE="https://raw.githubusercontent.com/${ORG}/${PROJECT}/${BRANCH}/pkg/apis/proto"
+echo "Remote URL prefix: $REMOTE_URL_PRE"
 
-curl -Ls -o ${REPO_ROOT}/dist/map.proto ${REMOTE_URL}/map/v1/map.proto
-curl -Ls -o ${REPO_ROOT}/dist/reduce.proto ${REMOTE_URL}/reduce/v1/reduce.proto
-curl -Ls -o ${REPO_ROOT}/dist/mapstream.proto ${REMOTE_URL}/mapstream/v1/mapstream.proto
-curl -Ls -o ${REPO_ROOT}/dist/sideinput.proto ${REMOTE_URL}/sideinput/v1/sideinput.proto
-curl -Ls -o ${REPO_ROOT}/dist/sink.proto ${REMOTE_URL}/sink/v1/sink.proto
-curl -Ls -o ${REPO_ROOT}/dist/source.proto ${REMOTE_URL}/source/v1/source.proto
-curl -Ls -o ${REPO_ROOT}/dist/transform.proto ${REMOTE_URL}/sourcetransform/v1/transform.proto
+curl -Ls -o ${REPO_ROOT}/dist/map.proto ${REMOTE_URL_PRE}/map/v1/map.proto
+curl -Ls -o ${REPO_ROOT}/dist/reduce.proto ${REMOTE_URL_PRE}/reduce/v1/reduce.proto
+curl -Ls -o ${REPO_ROOT}/dist/mapstream.proto ${REMOTE_URL_PRE}/mapstream/v1/mapstream.proto
+curl -Ls -o ${REPO_ROOT}/dist/sideinput.proto ${REMOTE_URL_PRE}/sideinput/v1/sideinput.proto
+curl -Ls -o ${REPO_ROOT}/dist/sink.proto ${REMOTE_URL_PRE}/sink/v1/sink.proto
+curl -Ls -o ${REPO_ROOT}/dist/source.proto ${REMOTE_URL_PRE}/source/v1/source.proto
+curl -Ls -o ${REPO_ROOT}/dist/transform.proto ${REMOTE_URL_PRE}/sourcetransform/v1/transform.proto
 
 protoc --go_out=module=github.com/numaproj/numaflow-go:. --go-grpc_out=module=github.com/numaproj/numaflow-go:. -I ${REPO_ROOT}/dist $(find ${REPO_ROOT}/dist -name '*.proto')
