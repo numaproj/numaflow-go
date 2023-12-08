e5b9f4da85463f73be34679294c812903c564133
Yashash H L <yashashhl25@gmail.com>
2023-12-08 12:09:18 +0530

    remove global reduce
    Signed-off-by: Yashash H L <yashashhl25@gmail.com>

--- pkg/apis/proto/globalreduce/v1/mockgen.go
+++
@@ -1,3 +0,0 @@
-package v1
-
-//go:generate mockgen -destination globalreducemock/globalreducemock.go -package globalreducemock github.com/numaproj/numaflow-go/pkg/apis/proto/globalreduce/v1 GlobalReduceClient,GlobalReduce_GlobalReduceFnClient
--- pkg/globalreducer/doc.go
+++
@@ -1,5 +0,0 @@
-// Package reducer implements the server code for reduce operation.
-
-// Examples: https://github.com/numaproj/numaflow-go/tree/main/pkg/reducer/examples/
-
-package globalreducer
