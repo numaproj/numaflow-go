dfe4935e589bba4996ea32b19bf17b572a1b849b
a3hadi <a3hadi@protonmail.com>
2024-03-11 15:47:13 -0400

    modify workflow to use matrix strategy
    Signed-off-by: a3hadi <a3hadi@protonmail.com>

--- update_examples.sh
+++ update_examples.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 function show_help () {
-    echo "Usage: $0 [-h|--help] (-bp|--build-push | -u|--update <SDK-version>)"
-    echo "  -h, --help          Display help message and exit"
-    echo "  -bp, --build-push   Build the docker images of all the examples and push them to the quay.io registry (with tag: stable)"
-    echo "  -u, --update        Update all of the examples to depend on the numaflow-go version with the specified SHA or version"
+    echo "Usage: $0 [-h|--help] (-bp|--build-push | -bpe|--build-push-example | -u|--update <SDK-version>)"
+    echo "  -h, --help                   Display help message and exit"
+    echo "  -bp, --build-push            Build the Dockerfiles of all the examples and push them to the quay.io registry (with tag: stable)"
+    echo "  -bpe, --build-push-example   Build the Dockerfile of the given example directory path, and push it to the quay.io registry (with tag: stable)"
+    echo "  -u, --update                 Update all of the examples to depend on the numaflow-go version with the specified SHA or version"
 }
 
 function traverse_examples () {
@@ -33,8 +34,10 @@ fi
 
 usingHelp=0
 usingBuildPush=0
+usingBuildPushExample=0
 usingVersion=0
 version=""
+directoryPath=""
 
 function handle_options () {
   while [ $# -gt 0 ]; do
@@ -44,6 +47,17 @@ function handle_options () {
         ;;
       -bp | --build-push)
         usingBuildPush=1
+        ;;
+      -bpe | --build-push-example)
+        usingBuildPushExample=1
+        if [ -z "$2" ]; then
+          echo "Directory path not specified." >&2
+          show_help
+          exit 1
+        fi
+
+        directoryPath=$2
+        shift
         ;;
       -u | --update)
         usingVersion=1
@@ -68,8 +82,8 @@ function handle_options () {
 
 handle_options "$@"
 
-if (( usingBuildPush + usingVersion + usingHelp > 1 )); then
-  echo "Only one of '-h', '-bp', '-u' is allowed at a time" >&2
+if (( usingBuildPush + usingBuildPushExample + usingVersion + usingHelp > 1 )); then
+  echo "Only one of '-h', '-bp', '-bpe, or, '-u' is allowed at a time" >&2
   show_help
   exit 1
 fi
@@ -78,8 +92,15 @@ if [ -n "$version" ]; then
  echo "Will update to: $version"
 fi
 
+if [ -n "$directoryPath" ]; then
+ echo "Dockerfile path to use: $directoryPath"
+fi
+
 if (( usingBuildPush )); then
   traverse_examples "make image"
+elif (( usingBuildPushExample )); then
+   cd "./$directoryPath" || exit
+   make image
 elif (( usingVersion )); then
   traverse_examples "go get github.com/numaproj/numaflow-go@$version" "go mod tidy"
 elif (( usingHelp )); then
