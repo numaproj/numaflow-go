3e766e91e3c0004a1eb58e3add9956ef01c8cbea
Abdullah Yildirim <a3hadi@protonmail.com>
2024-03-12 10:39:52 -0400

    fix: CI workflow (#104)
    Signed-off-by: a3hadi <a3hadi@protonmail.com>
    Co-authored-by: Keran Yang <yangkr920208@gmail.com>
    Signed-off-by: a3hadi <a3hadi@protonmail.com>

--- update_examples.sh
+++ update_examples.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 function show_help () {
-    echo "Usage: $0 [-h|--help] (-b|--build | -c|--commit-sha <commit_sha>)"
-    echo "  -h, --help         Display help message and exit"
-    echo "  -b, --build        Build the docker images of all the examples"
-    echo "  -c, --commit-sha   Update all of the examples to depend on the numaflow-go version with the specified SHA or version"
+    echo "Usage: $0 [-h|--help | -t|--tag <tag>] (-bp|--build-push | -bpe|--build-push-example <path> | -u|--update <SDK-version | commit-sha | latest >)"
+    echo "  -h, --help                   Display help message and exit"
+    echo "  -bp, --build-push            Build the Dockerfiles of all the examples and push them to the quay.io registry"
+    echo "  -bpe, --build-push-example   Build the Dockerfile of the given example directory path, and push it to the quay.io registry"
+    echo "  -t, --tag                    To be optionally used with -bpe or -bp. Specify the tag to build with. Default tag: stable"
+    echo "  -u, --update                 Update all of the examples to depend on the numaflow-go version with the specified SHA or version"
 }
 
 function traverse_examples () {
@@ -32,9 +34,13 @@ if [ $# -eq 0 ]; then
 fi
 
 usingHelp=0
-usingBuild=0
-usingSHA=0
-commit_sha=""
+usingBuildPush=0
+usingBuildPushExample=0
+usingVersion=0
+usingTag=0
+version=""
+directoryPath=""
+tag="stable"
 
 function handle_options () {
   while [ $# -gt 0 ]; do
@@ -42,18 +48,40 @@ function handle_options () {
       -h | --help)
         usingHelp=1
         ;;
-      -b | --build)
-        usingBuild=1
+      -bp | --build-push)
+        usingBuildPush=1
         ;;
-      -c | --commit-sha)
-        usingSHA=1
+      -bpe | --build-push-example)
+        if [ -z "$2" ]; then
+          echo "Directory path not specified." >&2
+          show_help
+          exit 1
+        fi
+
+        usingBuildPushExample=1
+        directoryPath=$2
+        shift
+        ;;
+      -t | --tag)
+        if [ -z "$2" ]; then
+          echo "Tag not specified." >&2
+          show_help
+          exit 1
+        fi
+
+        usingTag=1
+        tag=$2
+        shift
+        ;;
+      -u | --update)
         if [ -z "$2" ]; then
           echo "Commit SHA or version not specified." >&2
           show_help
           exit 1
         fi
 
-        commit_sha=$2
+        usingVersion=1
+        version=$2
         shift
         ;;
       *)
@@ -68,20 +96,40 @@ function handle_options () {
 
 handle_options "$@"
 
-if (( usingBuild + usingSHA + usingHelp > 1 )); then
-  echo "Only one of '-h', '-b', '-c' is allowed at a time" >&2
+if (( usingBuildPush + usingBuildPushExample + usingVersion + usingHelp > 1 )); then
+  echo "Only one of '-h', '-bp', '-bpe', or, '-u' is allowed at a time" >&2
   show_help
   exit 1
 fi
 
-if [ -n "$commit_sha" ]; then
- echo "Will update to: $commit_sha"
+if (( usingTag + usingVersion + usingHelp > 1 || (usingTag && usingBuildPush + usingBuildPushExample == 0) )); then
+  echo "Can only use -t with -bp or -bpe" >&2
+  show_help
+  exit 1
 fi
 
-if (( usingBuild )); then
-  traverse_examples "make image"
-elif (( usingSHA )); then
-  traverse_examples "go get github.com/numaproj/numaflow-go@$commit_sha" "go mod tidy"
+if [ -n "$version" ]; then
+ echo "Will update to: $version"
+fi
+
+if [ -n "$directoryPath" ]; then
+ echo "Dockerfile path to use: $directoryPath"
+fi
+
+if [ -n "$tag" ] && (( ! usingVersion )) && (( ! usingHelp )); then
+ echo "Using tag: $tag"
+fi
+
+if (( usingBuildPush )); then
+  traverse_examples "make image-push TAG=$tag"
+elif (( usingBuildPushExample )); then
+   cd "./$directoryPath" || exit
+   if ! make image-push TAG="$tag"; then
+     echo "Error: failed to run make image-push in $directoryPath" >&2
+     exit 1
+   fi
+elif (( usingVersion )); then
+  traverse_examples "go get github.com/numaproj/numaflow-go@$version" "go mod tidy"
 elif (( usingHelp )); then
   show_help
 fi
