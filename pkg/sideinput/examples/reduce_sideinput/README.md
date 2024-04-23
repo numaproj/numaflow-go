1b380fc492cef9c5d2ef12e958d760d8cb561f60
a3hadi <a3hadi@protonmail.com>
2024-04-23 11:02:05 -0400

    modify makefiles and dockerfiles
    Signed-off-by: a3hadi <a3hadi@protonmail.com>

---
+++ pkg/sideinput/examples/reduce_sideinput/README.md
@@ -0,0 +1,54 @@
+# Reduce SideInput Example
+An example that demonstrates how to write a [sideinput](https://numaflow.numaproj.io/user-guide/reference/side-inputs/) function along with a sample User Defined function which watches and used the corresponding side input with Mapper function.
+
+### SideInput
+```golang
+// handle is the side input handler function.
+func handle(_ context.Context) sideinputsdk.Message {
+    counter++
+    // BroadcastMessage() is used to broadcast the message with the given value to other side input vertices.
+    // val must be converted to []byte.
+    return sideinputsdk.BroadcastMessage([]byte(strconv.Itoa(counter)))
+}
+```
+
+By using [UDF](#user-defined-function), User perform the retrieval/update for the side input value and perform some operation based via reducer function on it and broadcast/drop a message to other vertices.
+
+### User Defined Function
+The UDF vertex will watch for changes to this file and whenever there is a change it will read the file to obtain the new side input value.
+
+### Pipeline spec
+In the spec we need to define the side input vertex and the UDF vertex. The UDF vertex will have the side input vertex as a side input.
+
+```yaml
+spec:
+  sideInputs:
+    - name: myticker
+      container:
+        image: "quay.io/numaio/numaflow-go/reduce-sideinput:stable"
+        imagePullPolicy: Always
+      trigger:
+        schedule: "@every 5s"
+```
+Vertex spec for the UDF vertex:
+```yaml
+    - name: si-e2e
+      udf:
+        container:
+          image: "quay.io/numaio/numaproj-contrib/reduce-sideinput-udf:stable"
+          imagePullPolicy: Always
+        groupBy:
+          window:
+            fixed:
+              length: 10s
+          keyed: true
+          storage:
+            persistentVolumeClaim:
+              volumeSize: 10Gi
+              accessMode: ReadWriteOnce
+      sideInputs:
+        - myticker
+```
+
+
+
---
+++ pkg/sideinput/examples/reduce_sideinput/udf/main.go
@@ -0,0 +1,128 @@
+/*
+Copyright 2023 The Numaproj Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"path"
+	"strconv"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+
+	"github.com/numaproj/numaflow-go/pkg/reducer"
+	"github.com/numaproj/numaflow-go/pkg/sideinput"
+)
+
+// SumReducerCreator implements the reducer.ReducerCreator interface which creates a reducer
+type SumReducerCreator struct {
+}
+
+func (s *SumReducerCreator) Create() reducer.Reducer {
+	return &Sum{}
+}
+
+// Sum is a reducer that sum up the values for the given keys
+type Sum struct {
+	sum int
+}
+
+var sideInputName = "myticker"
+var sideInputData []byte
+var mu sync.RWMutex
+
+func (s *Sum) Reduce(ctx context.Context, keys []string, reduceCh <-chan reducer.Datum, md reducer.Metadata) reducer.Messages {
+	mu.RLock()
+	siData := sideInputData
+	mu.RUnlock()
+
+	// sum up the values
+	for d := range reduceCh {
+		value, err := strconv.Atoi(string(d.Value()))
+		if err != nil {
+			log.Printf("unable to convert the value to integer: %v\n", err)
+			continue
+		}
+
+		s.sum += value
+	}
+
+	if s.sum > 0 && len(siData) > 0 {
+		msg := []byte("reduce_sideinput")
+		return reducer.MessagesBuilder().Append(reducer.NewMessage(msg))
+	} else {
+		return reducer.MessagesBuilder().Append(reducer.MessageToDrop())
+	}
+}
+
+func main() {
+	// Create a new fsnotify watcher
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	// Add a path to the watcher
+	err = watcher.Add(sideinput.DirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Start a goroutine to listen for events from the watcher
+	go fileWatcher(watcher, sideInputName)
+
+	err = reducer.NewServer(&SumReducerCreator{}).Start(context.Background())
+	if err != nil {
+		log.Panic("Failed to start reducer function server: ", err)
+	}
+}
+
+// fileWatcher will watch for any changes in side input file and set data in
+// sideInputData global variable.
+func fileWatcher(watcher *fsnotify.Watcher, sideInputName string) {
+	log.Println("Watching for changes in side input file: ", sideinput.DirPath)
+	p := path.Join(sideinput.DirPath, sideInputName)
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			if !ok {
+				log.Println("watcher.Events channel closed")
+				return
+			}
+			if event.Op&fsnotify.Create == fsnotify.Create && event.Name == p {
+				log.Println("Side input file has been created:", event.Name)
+				b, err := os.ReadFile(p)
+				if err != nil {
+					log.Println("Failed to read side input file: ", err)
+				}
+
+				// take a lock before updating sideInputData data to prevent race condition
+				mu.Lock()
+				sideInputData = b[:]
+				mu.Unlock()
+			}
+		case err, ok := <-watcher.Errors:
+			if !ok {
+				log.Println("watcher.Errors channel closed")
+				return
+			}
+			log.Println("error:", err)
+		}
+	}
+}
