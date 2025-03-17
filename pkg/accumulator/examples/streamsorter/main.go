afdbec65d14c250fdf1405c7426b7cc942300370
Vigith Maurice <vigith@gmail.com>
2025-03-17 08:21:16 -0700

    doc: add comments
    Signed-off-by: Vigith Maurice <vigith@gmail.com>

--- pkg/accumulator/examples/streamsorter/main.go
+++ pkg/accumulator/examples/streamsorter/main.go
@@ -9,60 +9,74 @@ import (
 	"github.com/numaproj/numaflow-go/pkg/accumulator"
 )
 
+// streamSorter buffers the incoming unordered (unordered by event-time but honors watermark) stream(s) and stores
+// in the sorted sortedBuffer.
 type streamSorter struct {
 	latestWm time.Time
-	buffer   []accumulator.Datum
+	// sortedBuffer stores the sorted data it has seen so far. The data stored will be completely sorted for
+	// (current watermark - 1) because it honors watermark.
+	sortedBuffer []accumulator.Datum
 }
 
+// Accumulate accumulates the read data in the sorted buffer and will emit the sorted results whenever the watermark
+// progresses.
 func (s *streamSorter) Accumulate(ctx context.Context, input <-chan accumulator.Datum, output chan<- accumulator.Message) {
 	for {
 		select {
 		case <-ctx.Done():
-			log.Println("Context done")
+			log.Println("Exiting the Accumulator")
 			return
 		case datum := <-input:
 			log.Println("Received datum with event time: ", datum.EventTime().UnixMilli())
+			// watermark has moved, let's flush
 			if datum.Watermark().After(s.latestWm) {
 				s.latestWm = datum.Watermark()
 				s.flushBuffer(output)
 			}
+			// store the data into the internal buffer
 			s.insertSorted(datum)
 		}
 	}
 }
 
+// insertSorted will do a binary-search and inserts the Datum into the internal sorted buffer.
 func (s *streamSorter) insertSorted(datum accumulator.Datum) {
-	index := sort.Search(len(s.buffer), func(i int) bool {
-		return s.buffer[i].EventTime().After(datum.EventTime())
+	index := sort.Search(len(s.sortedBuffer), func(i int) bool {
+		return s.sortedBuffer[i].EventTime().After(datum.EventTime())
 	})
-	s.buffer = append(s.buffer, datum)
-	copy(s.buffer[index+1:], s.buffer[index:])
-	s.buffer[index] = datum
+	s.sortedBuffer = append(s.sortedBuffer, datum)
+	copy(s.sortedBuffer[index+1:], s.sortedBuffer[index:])
+	s.sortedBuffer[index] = datum
 }
 
+// flushBuffer flushes the sorted results from the buffer into the outbound stream. While flushing it will
+// truncate the buffer up till <= (current watermark - 1).
 func (s *streamSorter) flushBuffer(output chan<- accumulator.Message) {
-	log.Println("Watermark updated, flushing buffer: ", s.latestWm.UnixMilli())
+	log.Println("Watermark updated, flushing sortedBuffer: ", s.latestWm.UnixMilli())
 	i := 0
-	for _, datum := range s.buffer {
+	for _, datum := range s.sortedBuffer {
 		if datum.EventTime().After(s.latestWm) {
 			break
 		}
 		output <- accumulator.MessageFromDatum(datum)
-		println("Sent datum with event time: ", datum.EventTime().UnixMilli())
+		log.Println("Sent datum with event time: ", datum.EventTime().UnixMilli())
 		i++
 	}
-	s.buffer = s.buffer[i:]
+
+	// truncate
+	s.sortedBuffer = s.sortedBuffer[i:]
 }
 
 type streamSorterCreator struct {
 }
 
+// Create creates an Accumulator for every key. It will be closed only when the timeout has expired.
 func (s *streamSorterCreator) Create() accumulator.Accumulator {
 	return &streamSorter{latestWm: time.UnixMilli(-1)}
 }
 
 func main() {
-	println("Starting server")
+	log.Println("Starting server")
 	err := accumulator.NewServer(&streamSorterCreator{}).Start(context.Background())
 	if err != nil {
 		log.Panic("Failed to start server: ", err)
