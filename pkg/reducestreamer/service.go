8c9bb5c0cabf42405c58a3c13e1d084e777ea4aa
adarsh0728 <gooneriitk@gmail.com>
2025-07-07 11:44:54 +0530

    error handling for reduce streamer
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- pkg/reducestreamer/service.go
+++ pkg/reducestreamer/service.go
@@ -55,39 +55,59 @@ func (fs *Service) ReduceFn(stream reducepb.Reduce_ReduceFnServer) error {
 		return nil
 	})
 
+	// Start a goroutine to receive messages from the gRPC stream and forward them to recvCh.
+	// Any error encountered (including io.EOF) is sent to recvErrCh.
+	recvCh := make(chan *reducepb.ReduceRequest, 1)
+	recvErrCh := make(chan error, 1)
+	go func() {
+		for {
+			d, err := stream.Recv()
+			if err != nil {
+				recvErrCh <- err
+				return
+			}
+			recvCh <- d
+		}
+	}()
+
+	// Main loop to process incoming requests and handle errors.
+	// Uses select to:
+	// - Process messages from the stream (via recvCh)
+	// - Handle errors from task goroutines (via taskManager.ErrorChannel())
+	// - Handle stream errors and EOF (via recvErrCh)
+	// This ensures the main loop remains responsive to both stream and task errors at all times.
+
 	// read messages from the stream and write the messages to corresponding channels
 	// if the channel is not created, create the channel and invoke the reduceFn
+loop:
 	for {
-		d, recvErr := stream.Recv()
-		// if EOF, close all the channels
-		if recvErr == io.EOF {
-			taskManager.CloseAll()
-			break
-		}
-		if recvErr != nil {
-			// the error here is returned by stream.Recv()
-			// it's already a gRPC error
+		select {
+		case errFromTask := <-taskManager.ErrorChannel():
+			fs.shutdownCh <- struct{}{}
+			return errFromTask
+		case d := <-recvCh:
+			switch d.Operation.Event {
+			case reducepb.ReduceRequest_WindowOperation_OPEN:
+				// create a new reduce task and start the reduce operation
+				err = taskManager.CreateTask(ctx, d)
+				if err != nil {
+					statusErr := status.Errorf(codes.Internal, "%s", err.Error())
+					return statusErr
+				}
+			case reducepb.ReduceRequest_WindowOperation_APPEND:
+				// append the datum to the reduce task
+				err = taskManager.AppendToTask(ctx, d)
+				if err != nil {
+					statusErr := status.Errorf(codes.Internal, "%s", err.Error())
+					return statusErr
+				}
+			}
+		case recvErr := <-recvErrCh:
+			if recvErr == io.EOF {
+				taskManager.CloseAll()
+				break loop
+			}
 			return recvErr
-		}
-
-		// for Aligned, its just open or append operation
-		// close signal will be sent to all the reducers when grpc
-		// input stream gets EOF.
-		switch d.Operation.Event {
-		case reducepb.ReduceRequest_WindowOperation_OPEN:
-			// create a new reduce task and start the reduce operation
-			err = taskManager.CreateTask(ctx, d)
-			if err != nil {
-				statusErr := status.Errorf(codes.Internal, "%s", err.Error())
-				return statusErr
-			}
-		case reducepb.ReduceRequest_WindowOperation_APPEND:
-			// append the datum to the reduce task
-			err = taskManager.AppendToTask(ctx, d)
-			if err != nil {
-				statusErr := status.Errorf(codes.Internal, "%s", err.Error())
-				return statusErr
-			}
 		}
 	}
 
