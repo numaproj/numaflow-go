f298f2b488a890d7ccccf6c05db05c6fa63bd5cf
Adarsh Jain <gooneriitk@gmail.com>
2025-08-21 09:09:29 +0530

    fix: client cancellation shouldn't trigger shutdown for source (#205)
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- pkg/sourcer/service.go
+++ pkg/sourcer/service.go
@@ -49,7 +49,8 @@ func (fs *Service) ReadFn(stream sourcepb.Source_ReadFnServer) error {
 	for {
 		if err := fs.receiveReadRequests(ctx, stream); err != nil {
 			// If the error is EOF, it means the stream has been closed.
-			if errors.Is(err, io.EOF) {
+			// Ignore client side cancellation.
+			if errors.Is(err, io.EOF) || errors.Is(err, context.Canceled) {
 				return nil
 			}
 			log.Printf("error processing read  requests: %v", err)
