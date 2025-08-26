b10895f3d19279de0782a77fb089d06fc5e84396
Adarsh Jain <gooneriitk@gmail.com>
2025-08-26 08:58:10 +0530

    chore: restructure examples directory and update them to use v0.10.1 (#206)
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- pkg/sideinput/doc.go
+++ pkg/sideinput/doc.go
@@ -1,5 +1,5 @@
 // package sideinput implements the server code for user-defined SideInputs in golang.
 
-// Examples: https://github.com/numaproj/numaflow-go/tree/main/pkg/sideinput/examples/
+// Examples: https://github.com/numaproj/numaflow-go/tree/main/examples/sideinput/
 
 package sideinput
