d9425955877f367585dfbbca3188b2a68177366a
adarsh0728 <gooneriitk@gmail.com>
2025-12-16 16:17:57 +0530

    transformer example
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- examples/sourcetransformer/assign_event_time/main.go
+++ examples/sourcetransformer/assign_event_time/main.go
@@ -15,18 +15,14 @@ type AssignEventTime struct {
 func (a *AssignEventTime) Transform(ctx context.Context, keys []string, d sourcetransformer.Datum) sourcetransformer.Messages {
 	// Update message event time to time.Now()
 	eventTime := time.Now()
-	umd := d.UserMetadata()
-	if umd != nil {
-		umd.AddKV("event-time-group", "event-time", []byte(eventTime.Format(time.RFC3339)))
-	}
 	// Log the action so that we can use it for testing.
-	// log.Printf("AssignEventTime: Assigning event time %v to message %s", eventTime, string(d.Value()))
-	return sourcetransformer.MessagesBuilder().Append(sourcetransformer.NewMessage(d.Value(), eventTime).WithKeys(keys).WithUserMetadata(umd))
+	log.Printf("AssignEventTime: Assigning event time %v to message %s", eventTime, string(d.Value()))
+	return sourcetransformer.MessagesBuilder().Append(sourcetransformer.NewMessage(d.Value(), eventTime).WithKeys(keys))
 }
 
 func main() {
 	err := sourcetransformer.NewServer(&AssignEventTime{}).Start(context.Background())
 	if err != nil {
-		log.Panic("Failed to start map function server: ", err)
+		log.Panic("Failed to start assign event time server: ", err)
 	}
 }
---
+++ examples/sourcetransformer/metadata_event_time/README.md
@@ -0,0 +1,3 @@
+# Metadata Event Time
+
+This is a simple User Defined Function example which receives a message, changes the message event time to a time.Now(), and adds key/value pair with key "event-time" and new event time value to event-time-group 
