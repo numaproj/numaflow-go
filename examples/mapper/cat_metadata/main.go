6c23b0a64b99e8db54b2978a3c60d59180ab193b
adarsh0728 <gooneriitk@gmail.com>
2025-12-17 09:40:43 +0530

    user NewUserMetadata
    Signed-off-by: adarsh0728 <gooneriitk@gmail.com>

--- examples/mapper/cat_metadata/main.go
+++ examples/mapper/cat_metadata/main.go
@@ -16,7 +16,7 @@ func (c *CatMetadata) Map(ctx context.Context, keys []string, d mapper.Datum) ma
 	// Print the user metadata for e2e tests in numaflow core
 	fmt.Println("Groups at mapper: ", um.Groups())
 	if um == nil {
-		um = &mapper.UserMetadata{}
+		um = mapper.NewUserMetadata()
 	}
 	um.CreateGroup("map-group")
 	um.AddKV("map-group", "map-key", []byte("map-value"))
